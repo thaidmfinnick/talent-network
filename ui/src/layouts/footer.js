b9984456339f5379439a37baa50011cbd9eef39a
hwt75 <dnthang300@gmail.com>
2024-11-26 19:42:24 +0700

    add header and footer

--- ui/src/layouts/footer.js
+++ ui/src/layouts/footer.js
@@ -2,8 +2,161 @@ import * as React from "react";
 
 export const Footer = () => {
   return (
-    <footer>
-
+    <>
+    {/* ======= Footer ======= */}
+    <footer id="footer" className="footer">
+      <div className="footer-content position-relative">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6">
+              <div className="footer-info">
+                <h3>UpConstruction</h3>
+                <p>
+                  A108 Adam Street <br />
+                  NY 535022, USA
+                  <br />
+                  <br />
+                  <strong>Phone:</strong> +1 5589 55488 55
+                  <br />
+                  <strong>Email:</strong> info@example.com
+                  <br />
+                </p>
+                <div className="social-links d-flex mt-3">
+                  <a
+                    href="#"
+                    className="d-flex align-items-center justify-content-center"
+                  >
+                    <i className="bi bi-twitter" />
+                  </a>
+                  <a
+                    href="#"
+                    className="d-flex align-items-center justify-content-center"
+                  >
+                    <i className="bi bi-facebook" />
+                  </a>
+                  <a
+                    href="#"
+                    className="d-flex align-items-center justify-content-center"
+                  >
+                    <i className="bi bi-instagram" />
+                  </a>
+                  <a
+                    href="#"
+                    className="d-flex align-items-center justify-content-center"
+                  >
+                    <i className="bi bi-linkedin" />
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* End footer info column*/}
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Useful Links</h4>
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About us</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Terms of service</a>
+                </li>
+                <li>
+                  <a href="#">Privacy policy</a>
+                </li>
+              </ul>
+            </div>
+            {/* End footer links column*/}
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Our Services</h4>
+              <ul>
+                <li>
+                  <a href="#">Web Design</a>
+                </li>
+                <li>
+                  <a href="#">Web Development</a>
+                </li>
+                <li>
+                  <a href="#">Product Management</a>
+                </li>
+                <li>
+                  <a href="#">Marketing</a>
+                </li>
+                <li>
+                  <a href="#">Graphic Design</a>
+                </li>
+              </ul>
+            </div>
+            {/* End footer links column*/}
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Hic solutasetp</h4>
+              <ul>
+                <li>
+                  <a href="#">Molestiae accusamus iure</a>
+                </li>
+                <li>
+                  <a href="#">Excepturi dignissimos</a>
+                </li>
+                <li>
+                  <a href="#">Suscipit distinctio</a>
+                </li>
+                <li>
+                  <a href="#">Dilecta</a>
+                </li>
+                <li>
+                  <a href="#">Sit quas consectetur</a>
+                </li>
+              </ul>
+            </div>
+            {/* End footer links column*/}
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Nobis illum</h4>
+              <ul>
+                <li>
+                  <a href="#">Ipsam</a>
+                </li>
+                <li>
+                  <a href="#">Laudantium dolorum</a>
+                </li>
+                <li>
+                  <a href="#">Dinera</a>
+                </li>
+                <li>
+                  <a href="#">Trodelas</a>
+                </li>
+                <li>
+                  <a href="#">Flexo</a>
+                </li>
+              </ul>
+            </div>
+            {/* End footer links column*/}
+          </div>
+        </div>
+      </div>
+      <div className="footer-legal text-center position-relative">
+        <div className="container">
+          <div className="copyright">
+            © Copyright{" "}
+            <strong>
+              <span>UpConstruction</span>
+            </strong>
+            . All Rights Reserved
+          </div>
+          <div className="credits">
+            {/* All the links in the footer should remain intact. */}
+            {/* You can delete the links only if you purchased the pro version. */}
+            {/* Licensing information: https://bootstrapmade.com/license/ */}
+            {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ */}
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>{" "}
+            Distributed by <a href="https://themewagon.com">ThemeWagon</a>
+          </div>
+        </div>
+      </div>
     </footer>
+  </>  
   );
 };
--- ui/src/layouts/header.js
+++ ui/src/layouts/header.js
@@ -1,7 +1,5 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import "./header.scss";
-
 
 export const Header = () => {
 
@@ -16,8 +14,91 @@ export const Header = () => {
   }, []);
 
   return (
-    <header>
-      
-    </header>
+    <>
+  {/* ======= Header ======= */}
+  <header id="header" className="header d-flex align-items-center">
+    <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="index.html" className="logo d-flex align-items-center">
+        {/* Uncomment the line below if you also wish to use an image logo */}
+        {/* <img src="assets/img/logo.png" alt=""> */}
+        <h1>
+          UpConstruction<span>.</span>
+        </h1>
+      </a>
+      <i className="mobile-nav-toggle mobile-nav-show bi bi-list" />
+      <i className="mobile-nav-toggle mobile-nav-hide d-none bi bi-x" />
+      <nav id="navbar" className="navbar">
+        <ul>
+          <li>
+            <a href="index.html" className="active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="about.html">About</a>
+          </li>
+          <li>
+            <a href="services.html">Services</a>
+          </li>
+          <li>
+            <a href="projects.html">Projects</a>
+          </li>
+          <li>
+            <a href="blog.html">Blog</a>
+          </li>
+          <li className="dropdown">
+            <a href="#">
+              <span>Dropdown</span>{" "}
+              <i className="bi bi-chevron-down dropdown-indicator" />
+            </a>
+            <ul>
+              <li>
+                <a href="#">Dropdown 1</a>
+              </li>
+              <li className="dropdown">
+                <a href="#">
+                  <span>Deep Dropdown</span>{" "}
+                  <i className="bi bi-chevron-down dropdown-indicator" />
+                </a>
+                <ul>
+                  <li>
+                    <a href="#">Deep Dropdown 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Dropdown 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Dropdown 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Dropdown 4</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Dropdown 5</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Dropdown 2</a>
+              </li>
+              <li>
+                <a href="#">Dropdown 3</a>
+              </li>
+              <li>
+                <a href="#">Dropdown 4</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      {/* .navbar */}
+    </div>
+  </header>
+  {/* End Header */}
+</>
+
   );
 };
