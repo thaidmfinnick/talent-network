3480a61ca689a455740c89794a2f3d0d6965a853
hwt75 <dnthang300@gmail.com>
2024-12-01 20:48:07 +0700

    issue #6: add language

--- ui/src/layouts/footer.js
+++ ui/src/layouts/footer.js
@@ -137,25 +137,6 @@ export const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="footer-legal text-center position-relative">
-        <div className="container">
-          <div className="copyright">
-            © Copyright{" "}
-            <strong>
-              <span>UpConstruction</span>
-            </strong>
-            . All Rights Reserved
-          </div>
-          <div className="credits">
-            {/* All the links in the footer should remain intact. */}
-            {/* You can delete the links only if you purchased the pro version. */}
-            {/* Licensing information: https://bootstrapmade.com/license/ */}
-            {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/ */}
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>{" "}
-            Distributed by <a href="https://themewagon.com">ThemeWagon</a>
-          </div>
-        </div>
-      </div>
     </footer>
   </>  
   );
