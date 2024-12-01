19454d3d026f067258d1a959b7f49a83836a0689
hwt75 <dnthang300@gmail.com>
2024-12-01 11:00:16 +0700

    issue#6: config multiple language

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter} from "react-router-dom";
 import { Layout } from "./layouts/layout";
+import './config'
 export const App = () => {
   return (
       <BrowserRouter>
